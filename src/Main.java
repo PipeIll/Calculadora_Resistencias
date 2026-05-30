3a12dbb6b88a1f8e20ccde4ca438f2de97ffd05c
FELIPE <aparocio07felipe@gmail.com>
2026-05-29 20:29:55 -0500

    Fix: main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import UI.Ventana;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ignored) {}
+
+            new Ventana().setVisible(true);
+        });
+    }
+}
